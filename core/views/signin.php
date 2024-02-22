cd7e1552596b19c6b6009a9c02d2155465fbc9ae
Weler S Martins <welerson194@gmail.com>
2024-02-22 15:16:12 -0300

    p

--- core/views/signin.php
+++ core/views/signin.php
@@ -1,29 +1,29 @@
 <div class=" vh-100 " style="margin-top:100px">
 
     <div class="welcome-container  w-100">
-        <?php if (isset($_SESSION['error'])) :  ?>
 
-            <div class='alert alert-danger text-center'>
-                <?= $_SESSION['error'] ?>
-                <?php unset($_SESSION['error']) ?>
-            </div>
-
-        <?php endif; ?>
-
-        <?php if (isset($_SESSION['success'])) :  ?>
-
-            <div class='alert alert-success text-center'>
-                <?= $_SESSION['success'] ?>
-                <?php unset($_SESSION['success']) ?>
-            </div>
-
-        <?php endif; ?>
         <form class="login-form p-4  position-absolute " style="width:400px;top:50%;left:50%;transform: translate(-50%, -50%)" action="?a=signin" method="POST">
 
 
 
+            <?php if (isset($_SESSION['error'])) :  ?>
 
-            
+                <div class='alert alert-danger text-center'>
+                    <?= $_SESSION['error'] ?>
+                    <?php unset($_SESSION['error']) ?>
+                </div>
+
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['success'])) :  ?>
+
+                <div class='alert alert-success text-center'>
+                    <?= $_SESSION['success'] ?>
+                    <?php unset($_SESSION['success']) ?>
+                </div>
+
+            <?php endif; ?>
+
 
             <p class="text-center text-success  fs-2 mb-4">Login</p>
 
